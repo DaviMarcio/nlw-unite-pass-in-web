89f3cade01657900d85965166b40a4e9e98b1fd3
Ghostz <davimarcio532@gmail.com>
2024-04-06 13:55:20 -0300

    :tada: Initial Commit

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from "react"
+
+interface NavlinkProps extends ComponentProps<'a'>{
+  children: string
+  href: string
+}
+
+export function Navlink(props: NavlinkProps) {
+  return(
+    <a {...props} className='font-medium text-sm'>
+      {props.children}
+    </a>
+  )
+}
